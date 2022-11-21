3b33992a460a353cc30b875ae838a81d269e7b98
Candela Iara Martinez Burgos <violecandeiara@gmail.com>
2022-11-21 11:13:39 -0300

    cambios

---
+++ nombre-app/src/components/CartWidgets/CartWidgets.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from '../icons/Cart';
+
+const CartWidgets = () => {
+  return (
+    <div>
+        <Cart />
+    </div>
+  )
+}
+
+export default CartWidgets
---
+++ nombre-app/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = (peep) => {
+  return (
+    <div>Bienvenido {peep.name}</div>
+  )
+}
+
+export default ItemListContainer
