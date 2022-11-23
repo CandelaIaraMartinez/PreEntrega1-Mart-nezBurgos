473c38e1a98ed1e1257e08f654e63a72486f28af
Candela Iara Martinez Burgos <violecandeiara@gmail.com>
2022-11-23 19:53:55 -0300

    cambios

--- nombre-app/src/App.js
+++ nombre-app/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import './App.css';
 
 function App() {
   return (
   <>
     <NavBar />
-    <ItemListContainer />
+    <ItemListContainer name='Pablo'/>
   </>
   )
 }
--- nombre-app/src/components/NavBar/NavBar.jsx
+++ nombre-app/src/components/NavBar/NavBar.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import CartWidgets from '../CartWidgets/CartWidgets';
-import '../../App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {AiOutlineShoppingCart} from "react-icons/ai";
 
-const NavBar = () => {
+function NavBar() {
   return (
-    <div>
-      <nav>
-        Librería Constelaciones
-      <ul class="App-header">
-        <li>Home</li>
-        <li>Libros</li>
-        <li>Búsqueda</li>
-      </ul>
-      <CartWidgets />
-      </nav>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Libreria Constelaciones</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">Libros populares</Nav.Link>
+            <Nav.Link href="#">Reseñas</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Búsqueda</Button>
+          </Form>
+        </Container>
+          <CartWidgets />
+      </Navbar>
+      <br />
+    </>
   );
 }
 
