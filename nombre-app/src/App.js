fd78438dbb707b26a3380892b9103b985a63f505
Candela Iara Martinez Burgos <violecandeiara@gmail.com>
2022-11-23 18:33:52 -0300

    cambios

--- nombre-app/src/App.js
+++ nombre-app/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-  <div>
+  <>
     <NavBar />
     <ItemListContainer />
-  </div>
+  </>
   )
 }
 
