82d9eda932a543cdc44d06d1862e0db5427a029c
Candela Iara Martinez Burgos <violecandeiara@gmail.com>
2022-12-06 11:44:29 -0300

    interactuando con apis

--- nombre-app/src/components/NavBar/NavBar.jsx
+++ nombre-app/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import CartWidgets from '../CartWidgets/CartWidgets';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {AiOutlineShoppingCart} from "react-icons/ai";
@@ -17,15 +15,6 @@ function NavBar() {
             <Nav.Link href="#">Libros populares</Nav.Link>
             <Nav.Link href="#">Reseñas</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Búsqueda</Button>
-          </Form>
         </Container>
           <CartWidgets />
       </Navbar>
