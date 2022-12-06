3e93650fea3969eb4dfcdb4f7a9f82e37a480ac6
Candela Iara Martinez Burgos <violecandeiara@gmail.com>
2022-12-06 11:44:42 -0300

    interactuando con apis

--- nombre-app/src/components/ItemListContainer/ItemListContainer.jsx
+++ nombre-app/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import Alert from 'react-bootstrap/Alert';
+import React, { useEffect, useState } from "react";
 
-const ItemListContainer = (props) => {
+const API = "http://www.omdbapi.com/?i=tt3896198&apikey=952fbfe6";
+
+const ItemListContainer = () => {
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+    searchTerm: "",
+    error: "",
+  });
+
+  const getPelicula = async () => {
+    const res = await fetch(`${API}&s=robot`);
+    const resJSON = await res.json();
+
+    if (resJSON) {
+      setState({
+        data: resJSON.Search,
+        loading: false,
+        error: "",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getPelicula();
+  }, []);
+
+  const mostrarPelicula = async (e) => {
+    e.preventDefault();
+  
+    const response = await fetch(`${API}&s=${state.searchTerm}`);
+    const data = await response.json();
+
+    return setState({
+      data: data.Search,
+      searchTerm: "",
+      error: "",
+    });
+  };
+
   return (
-    <Alert key="success" variant="success">
-        Bienvenido {props.name}
-    </Alert>
-  )
-}
+    <>
+      <div className="row">
+        <div className="col-md-4 offset-md-4 p-4">
+          <form onSubmit={mostrarPelicula}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Buscar pelicula"
+              onChange={(e) => setState({...state, searchTerm: e.target.value })}
+              value={state.searchTerm}
+            />
+          </form>
+          <p className="text-white">{state.error ? state.error : ""}</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ItemListContainer
